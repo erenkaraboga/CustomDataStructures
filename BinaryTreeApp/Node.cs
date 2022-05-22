65d0e6b2e5ddde5b91bb93ab7430aee5133766fb
Eren Karaboğa <darkbull1073@gmail.com>
2022-05-22 10:55:39 +0300

    Node class Designed

---
+++ BinaryTreeApp/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeApp
+{
+    public class Node<T>
+    {
+        public Node(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+        public override string ToString()
+        {
+            return $"{Value}";
+        }
+    }
+}
---
+++ BinaryTreeApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BinaryTreeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new Node<int>(1);
+            root.Left = new Node<int>(2);
+            root.Right = new Node<int>(3);
+            root.Left.Left = new Node<int>(4);
+            Console.WriteLine(root);
+        }
+    }
+}
