81f84c358adb3859440c4eeda4b90ea35a2345da
Eren Karaboğa <darkbull1073@gmail.com>
2022-05-22 11:40:06 +0300

    Level Order Traverse Implemented

--- BinaryTreeApp/Program.cs
+++ BinaryTreeApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BinaryTreeApp
 {
@@ -10,7 +11,27 @@ namespace BinaryTreeApp
             root.Left = new Node<int>(2);
             root.Right = new Node<int>(3);
             root.Left.Left = new Node<int>(4);
+            root.Left.Right = new Node<int>(5);
+            root.Right.Left = new Node<int>(6);
+            root.Right.Right = new Node<int>(7);
             Console.WriteLine(root);
+        }
+        static void levelOrderTraverse<T>(Node<T> root)
+        {
+            var list = new List<Node<T>>();
+            if (root != null)
+            {
+                var q = new Queue<Node<T>>();
+                q.Enqueue(root);
+                while (q.Count>0)
+                {
+                    var temp = q.Dequeue();
+                    list.Add(temp);
+                    if(temp.Left!= null) q.Enqueue(temp.Left);
+                    if (temp.Right != null) q.Enqueue(temp.Right);
+                }
+            }
+
         }
     }
 }
